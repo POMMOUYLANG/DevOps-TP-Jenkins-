53b8bcd4558ae9f01db62f39bedb71176d8590b2
POMMOUYLANG <mouylangpom@gamail.com>
2024-03-20 22:06:06 +0700

    My_TP01:DevOps

---
+++ src/main/java/gic/demo/App.java
@@ -0,0 +1,15 @@
+package gic.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "My name POM MOUYLANG!" );
+        System.out.println( "I'm 22 years old!" );
+    }
+}
