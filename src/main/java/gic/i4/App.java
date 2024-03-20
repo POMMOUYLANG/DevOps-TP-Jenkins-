caa573a34d9cdc149d1a56beb3873a6bb48ccbf5
POMMOUYLANG <mouylangpom@gamail.com>
2024-03-20 22:03:54 +0700

    DevOps:Jenkins-Test

---
+++ src/main/java/gic/i4/App.java
@@ -0,0 +1,14 @@
+package gic.i4;
+
+/**
+ * I4 TP 1 Autobuild trigger
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I4 TP 1 Autobuild trigger every minute" );
+        System.out.println("Happy birthday"+" toYou");
+    }
+}
